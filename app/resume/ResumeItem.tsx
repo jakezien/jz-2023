0f0441595174f6d22ac2762c6ab3ed962923ac83
jakezien <jakezien@hey.com>
2023-09-11 13:27:58 -0400

    initial commit, json data flowing through to resume page

---
+++ app/resume/ResumeItem.tsx
@@ -0,0 +1,30 @@
+export type ResumeItem = {
+  id: number;
+  title: string;
+  company?: string;
+  startDate: string;
+  endDate: string;
+  description?: string[];
+  achievements?: string[];
+};
+
+type Props = {
+    item: ResumeItemType;
+};
+
+const ResumeItemComponent: React.FC<Props> = ({ item }) => {
+    return (
+        <div>
+            <h2>{item.title}</h2>
+            <h3>{item.company}</h3>
+            <p>{item.description}</p>
+            <ul>
+                {item.achievements?.map((achievement, index) => (
+                    <li key={index}>{achievement}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ResumeItemComponent;
---
+++ app/resume/page.tsx
@@ -0,0 +1,35 @@
+import { ResumeItem } from './ResumeItem'
+import ResumeItemComponent from './ResumeItem';
+
+import fsPromises from 'fs/promises';
+import path from 'path'
+
+type Props = {
+  resumeItems: ResumeItem[];
+};
+
+export async function getLocalData(): Promise<ResumeItem[]> {
+  const filePath = path.join(process.cwd(), 'public/resume.json');
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData: ResumeItem[] = JSON.parse(jsonData.toString());
+  return objectData;
+}
+
+
+async function ResumePage() {
+  const resumeItems = await getLocalData()
+  console.log("poop" + resumeItems.length)
+
+  return (
+    <div>
+      <h1>Hey ho</h1>
+      {resumeItems.map(item => (
+        <ResumeItemComponent key={item.id} item={item}/>
+      ))} 
+    </div>
+  )
+}
+
+
+
+export default ResumePage;
