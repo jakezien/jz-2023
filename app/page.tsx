94e2704ae699d71dcf88b2f34336430e12873366
jakezien <jakezien@hey.com>
2023-09-12 14:05:03 -0400

    spacing

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-24">
-      <div className='text-center pb-8'>
+      <div className='text-center pb-4 md:pb-8 lg:pb-12'>
         <Image src={monogramImage} alt="JZ monogram" width={160} height={160} className='rounded-xl mx-auto mb-6'></Image>
         <h1 className={h1ClassName}><Link href="/resume" className="cursor-pointer underline underline-offset-4 text-designOrange hover:text-orange-600 active:text-orange-700">My resume</Link></h1>
         <h1 className={h1ClassName}><a className='cursor-pointer underline underline-offset-4 text-designOrange hover:text-orange-600 active:text-orange-700' href={"mailto:jz@jakezien.com"}>Get in touch</a></h1>
