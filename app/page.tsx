2e70baa2ffa4b5531b6d00a85dbec00a400c8405
jakezien <jakezien@hey.com>
2023-09-12 14:03:59 -0400

    mobile finesse

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   let h1ClassName = plex.className + " font-bold tracking-normal my-2 cursor-pointer"
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center py-24">
       <div className='text-center pb-8'>
-        <Image src={monogramImage} alt="JZ monogram" width={240} height={240} className='rounded-xl mx-auto mb-6'></Image>
+        <Image src={monogramImage} alt="JZ monogram" width={160} height={160} className='rounded-xl mx-auto mb-6'></Image>
         <h1 className={h1ClassName}><Link href="/resume" className="cursor-pointer underline underline-offset-4 text-designOrange hover:text-orange-600 active:text-orange-700">My resume</Link></h1>
         <h1 className={h1ClassName}><a className='cursor-pointer underline underline-offset-4 text-designOrange hover:text-orange-600 active:text-orange-700' href={"mailto:jz@jakezien.com"}>Get in touch</a></h1>
         <h2 className='mt-4 font-medium text-stone-400'>Stay tuned for more</h2>
