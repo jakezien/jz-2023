e5a6ed7f4592ffd17103ce364ff1f38b2b7a24cb
jakezien <jakezien@hey.com>
2023-10-12 20:42:55 -0400

    style for gallery

--- app/components/ProjectGallery.tsx
+++ app/components/ProjectGallery.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import { useProjects } from "../context/ProjectContext";
 import Image from "next/image";
 import { IBM_Plex_Mono } from "next/font/google"
+import { Inter } from "next/font/google"
 import { useEffect } from "react";
 const plex = IBM_Plex_Mono({ weight: ['700'], subsets: ['latin'] })
+// const inter = Inter({ weight: ['500'], subsets: ['latin'] })
 
 interface Props { }
 
@@ -26,19 +28,19 @@ const ProjectsGallery: React.FC<Props> = ({ }) => {
                 {org.description}
               </p>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             {orgProjects.map((project, index) => (
               <Link href={`/projects/${org.slug}-${project.slug}`} key={index}>
-                <div className="overflow-hidden mb-4">
+                <div className="overflow-hidden bg-white p-3 pb-6 rounded-md shadow-sm transition-all hover:scale-[101%] hover:shadow-md ease-in-out duration-200">
                   <Image
                     src={project.images[0].src}
                     alt={project.images[0].alt}
                     width={1000}
                     height={750}
-                    className="aspect-4/3 object-cover mb-2 transition-opacity opacity-0 duration-500"
+                    className="aspect-4/3 object-cover mb-2 transition-opacity opacity-0 duration-500 rounded overflow-hidden"
                     onLoadingComplete={(image) => {image.classList.remove('opacity-0')}}
                   />
-                  <p>{project.title}</p>
+                  <p className={"text-lg font-medium "}>{project.title}</p>
                 </div>
               </Link>
             ))}
